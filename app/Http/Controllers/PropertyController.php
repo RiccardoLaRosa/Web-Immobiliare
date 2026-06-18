83be4dca3e95c8b2f7e27497d80a693f8eb01e6c
Riccardo La Rosa <riccardofedericolarosa@gmail.com>
2026-06-18 19:12:28 +0200

    Aggiunta logica di visualizzazione show nel controller, refactoring della vista blade show, fix scritta login

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -40,7 +40,7 @@ class PropertyController extends Controller
      */
     public function show(Property $property)
     {
-        //
+        return view('properties.show', compact('property'));
     }
 
     /**
